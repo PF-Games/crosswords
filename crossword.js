4b8f6a68a38a048695c7bf4c36705e3306c91bb2
PFGames <132846673+PF-Games@users.noreply.github.com>
2024-12-06 11:52:31 -0300

    Crates a function searching the last index
    A function searching a specific item in the array can be used in our crossword to search for letter in specific locations of a word.

---
+++ crossword.js
@@ -0,0 +1,16 @@
+/* FUNCIÓN IMPRIMIR ÚLTIMO ELEMENTO DE UN ARRAY
+
+let supplies = [
+    'dive mask',
+    'water wshoes',
+    'fins',
+    'scuba tank'
+];
+
+
+function printLastItem(){
+    let lastIndex = supplies.length -1;
+    console.log(supplies[lastIndex]);
+}
+
+*/
