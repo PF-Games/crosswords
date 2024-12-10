058dfe30e8289888a61bbe2c21c9243180a03018
PFGames <132846673+PF-Games@users.noreply.github.com>
2024-12-10 16:45:46 -0300

    Creates a function to map a word
    Creating an object and storing the letters as properties allow for more efficient solutions than simply searching along the whole strings of every word in the dictionary

--- crossword.js
+++ crossword.js
@@ -199,7 +199,9 @@ console.log(findLongestWord(dict));
 
 */
 
+/////////////////////////////////////////////////////////
 
+/*
 function findRepeatedWords(array) {
     let wordCount = {};
     let repeatedWords = [];
@@ -221,3 +223,18 @@ function findRepeatedWords(array) {
 
 
 console.log(findRepeatedWords(dict)); 
+*/
+
+
+function mapString(string){
+    let map = {};
+    for (let i = 0; i < string.length; i++){
+        let letter = string[i];
+        if(map[letter]) {
+            map[letter].push(i);
+        }else{
+            map[letter] = [i];
+        }
+    }
+    return map;
+}
